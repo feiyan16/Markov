7cf791ccc6cc5a3163cf2ca794ace049880093b1
feiyan16 <feiyan.su16@gmail.com>
2021-10-07 13:24:29 -0500

    added print method and test files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 
 class State:
@@ -7,6 +8,7 @@ class State:
         self.reward = reward
         self.actions: dict = {}
         self.optimal_a = -1
+        self.j = -1
 
     def add_action(self, action_id, action_s, action_prob):
         if action_id in self.actions.keys():
@@ -43,33 +45,48 @@ def read_file(filename):
     return states_
 
 
-print_format = "(s{} a{} {:.4f}) "
-states = read_file("/Users/feiyansu/Desktop/test2.in")
-iteration = []
-print_str = "After iteration 1: "
-# base cases, at t = 1
-for state in states:
-    iteration.insert(state.id, state.reward)
-    rand_a = random.randint(1, len(state.actions))
-    print_str += (print_format.format(state.id, rand_a, state.reward))
-print(print_str)
-# remaining cases from 2-20
-for t in range(1, 20):
-    print_str = "After iteration " + str(t+1) + ": "
-    iteration_t = []
-    for state in states:
-        max_a = -1000
-        opt_a = None
-        for action, state_prob in state.actions.items():
-            x_val = 0
-            for sp in state_prob:
-                prob = sp[1]
-                j = iteration[sp[0]-1]
-                x_val += (prob * j)
-            max_a = max(max_a, x_val)
-            if max_a == x_val:
-                opt_a = action
-        iteration_t.insert(state.id, state.reward + 0.9 * max_a)
-        print_str += (print_format.format(state.id, opt_a, iteration_t[state.id - 1]))
-    iteration = iteration_t.copy()
+def printer(updated_states, it_t):
+    print_str = "After iteration " + str(it_t) + ": "
+    for s in updated_states:
+        print_str += ("(s{} a{} {:.4f}) ".format(s.id, s.optimal_a, s.j))
     print(print_str)
+
+
+def bellman_calculator(g, states):
+    iteration = []
+    # base cases, at t = 1
+    for state in states:
+        iteration.insert(state.id, state.reward)
+        rand_a = random.randint(1, len(state.actions))
+        state.optimal_a = rand_a
+        state.j = state.reward
+    printer(states, 1)
+    # remaining cases from 2-20
+    for t in range(1, 20):
+        iteration_t = []
+        for state in states:
+            max_a = -1000
+            for action, state_prob in state.actions.items():
+                x_val = 0
+                for sp in state_prob:
+                    prob = sp[1]
+                    j = iteration[sp[0] - 1]
+                    x_val += (prob * j)
+                max_a = max(max_a, x_val)
+                if max_a == x_val:
+                    state.optimal_a = action
+            state.j = state.reward + g * max_a
+            iteration_t.insert(state.id, state.j)
+        printer(states, t+1)
+        iteration = iteration_t.copy()
+
+
+n = len(sys.argv)
+if n < 5:
+    print("python3 main.py <number of states> <max actions> <input file path> <discount factor>")
+    quit()
+elif n == 5:
+    gamma = sys.argv[4]
+    file_path = sys.argv[3]
+    all_states = read_file(file_path)
+    bellman_calculator(float(gamma), all_states)
